41dd7d8e1b980cd0590475c1ab88d6726b8f4caa
AnkitWasnik <ankit.wasnik@talentica.com>
2020-08-02 10:13:52 +0530

    Init

---
+++ src/test/java/com/ankitwasnik/rabbitmq/consumers/RabbitmqConsumersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankitwasnik.rabbitmq.consumers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqConsumersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
